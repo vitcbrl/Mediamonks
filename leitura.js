3f4dd05e0ab483b44d1897718b6998f57b5d4db7
vitoria cabral <vitoria.oliveira@alu.ufc.br>
2024-01-22 22:45:11 -0300

    Criar uma função em JavaScript para ler os arquivos JSON

--- leitura.js
+++ leitura.js
@@ -10,3 +10,10 @@ function lerArquivoJson(nomeArquivo) {
     return null;
   }
 }
+
+// Exemplo:
+const database1 = lerArquivoJson('broken_database_1.json');
+const database2 = lerArquivoJson('broken_database_2.json');
+
+console.log('Database 1:', database1);
+console.log('Database 2:', database2);
